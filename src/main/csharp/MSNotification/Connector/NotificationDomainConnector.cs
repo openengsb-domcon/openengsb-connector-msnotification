40eb77a350e989c226a85149f9febcb4c300187a
pgrm <peter.grman@gmail.com>
2012-07-18 01:10:13 +0200

    Added a simple implementation of the NotificationDomain

---
+++ src/main/csharp/MSNotification/Connector/NotificationDomainConnector.cs
@@ -0,0 +1,20 @@
+﻿namespace Org.OpenEngSB.Connector.MSNotification.Connector
+{
+    using System;
+    using Org.OpenEngSB.Connector.MSNotification.Common;
+    using Org.OpenEngSB.Domain.Notification;
+
+    public class NotificationDomainConnector : INotificationDomainSoap11Binding
+    {
+        public event EventHandler<SimpleEventArgs<Notification>> Notified;
+
+        #region INotificationDomainSoap11Binding Members
+
+        public void notify(Notification args0)
+        {
+            Notified.SafeInvoke(this, new SimpleEventArgs<Notification>(args0));
+        }
+
+        #endregion
+    }
+}
