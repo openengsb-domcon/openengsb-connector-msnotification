abd6006314ea6d43947550f34ba9f036b829db62
pgrm <peter.grman@gmail.com>
2012-07-18 01:06:04 +0200

    added custom balloon and icons

---
+++ src/main/csharp/MSNotification/GUI/Controls/CustomBalloon.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Org.OpenEngSB.Connector.MSNotification.GUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for CustomBalloon.xaml
+    /// </summary>
+    public partial class CustomBalloon : UserControl
+    {
+        public CustomBalloon()
+        {
+            InitializeComponent();
+        }
+    }
+}
